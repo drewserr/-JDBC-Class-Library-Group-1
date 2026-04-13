06b05636d3ce54f0d36a52e575456257fcdfc7ea
Andrew Laboy <drewserrr@gmail.com>
2026-04-13 14:57:26 -0400

    Updated files

--- src/main/java/jdbc/dao/GenericDAO.java
+++ src/main/java/jdbc/dao/GenericDAO.java
@@ -2,12 +2,6 @@ package jdbc.dao;
 
 import java.util.List;
 
-/**
- * Generic CRUD contract for DAO classes.
- *
- * @param <T>  entity type
- * @param <ID> primary key type
- */
 public interface GenericDAO<T, ID> {
     boolean insert(T entity);
     T findById(ID id);
